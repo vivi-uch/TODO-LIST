e6fef50b417b8d71592a87e0d7347b0458046811
Vivian Okonkwo <167189224+vivi-uch@users.noreply.github.com>
2024-05-04 04:15:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -227,10 +227,10 @@ pending.addEventListener("click",()=>{
 theme.onclick = function(){
     let check = document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        icon.src = "/images/sun.png";
+        icon.src = "images/sun.png";
     }
     else{
-        icon.src = "/images/moon.png";
+        icon.src = "images/moon.png";
     }
 }
 
